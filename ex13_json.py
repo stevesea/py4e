2d89dcfb48b04b07ce7b5b2db71800e482f7798c
Steve Christensen <steve.christensen@mediciventures.com>
2018-07-12 18:15:12 -0600

    json parsing

---
+++ ex13_json.py
@@ -0,0 +1,25 @@
+import json
+import urllib.request, urllib.parse, urllib.error
+
+address = input('Enter location: ')
+if len(address) < 1:
+    import sys
+    sys.exit(1)
+
+print('Retrieving', address)
+uh = urllib.request.urlopen(address)
+data = uh.read()
+print('Retrieved', len(data), 'characters')
+#print(data.decode())
+
+jsonData = json.loads(data)
+
+total = 0
+num_commenters = 0
+
+for commenter in jsonData['comments']:
+    total += commenter['count']
+    num_commenters += 1
+
+print("Count:", num_commenters)
+print("Sum:", total)
